7d3992702f611dcc567fa8a098f8baa33fde55c3
yahel8666 <yahelcamisay@gmail.com>
2024-07-16 00:08:39 -0300

    ayuda

--- servicioApi.cs
+++ servicioApi.cs
@@ -2,21 +2,22 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
-namespace miProyecto;
 using WebApiProyecto; 
+namespace miProyecto;
 
 public class MiApi()
 {
-public static async Task<UsuarioAleatorio> GetGeneraNombre()
-{
+    public static async Task<UsuarioAleatorio> GetGeneraUsuario()
+    {
 
-var url = "https://random-data-api.com/api/v2/users?size=1";
+        var url = "https://random-data-api.com/api/v2/users?size=1";
 
-HttpClient client = new HttpClient();
-HttpResponseMessage response = await client.GetAsync(url);
-response.EnsureSuccessStatusCode();
-string responseBody = await response.Content.ReadAsStringAsync();
-UsuarioAleatorio usuario = JsonSerializer.Deserialize<UsuarioAleatorio>(responseBody);
-return usuario;
-}
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        
+        string responseBody = await response.Content.ReadAsStringAsync();
+        UsuarioAleatorio usuario = JsonSerializer.Deserialize<UsuarioAleatorio>(responseBody);
+        return usuario;
+    }
 }
